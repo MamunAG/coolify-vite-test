6481df02ca8a25fa759b959be51891a0c35b77d4
MamunAG <mamun166036@yahoo.com>
2026-02-16 15:51:17 +0600

    Fix typo in HMR test message

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR...235
+          Edit <code>src/App.tsx</code> and save to test HMR...235vv
         </p>
       </div>
       <p className="read-the-docs">
